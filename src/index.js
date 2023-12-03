9141ab764a27ddf3d13f41369561d5b375758159
champkeh <champion.ge@gmail.com>
2023-12-03 13:47:27 +0800

    add plugins

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-const parser = require('@babel/parser')
-const traverse = require('@babel/traverse').default
-const generate = require('@babel/generator').default
-const types = require('@babel/types')
-const template = require('@babel/template').default
 const fs = require('fs')
 const path = require('path')
-const Visitors = require('./transformations')
 const babel = require('@babel/core')
 const preset = require('./plugins/preset')
 
